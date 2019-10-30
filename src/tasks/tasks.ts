9ece98c90084d3529c536d0b10fa92b7137cb67b
Mikhail Nasyrov <streambot@gmail.com>
2019-10-31 00:53:49 +0700

    refactor: Simplified state updating.

--- src/tasks/tasks.ts
+++ src/tasks/tasks.ts
@@ -1,5 +1,4 @@
 import { guid, Query, Store } from '@datorama/akita';
-import { stateProduce } from '../utils';
 
 export interface Task {
   id: string;
@@ -35,29 +34,27 @@ export class TaskService {
   createTask(title: string): string {
     const id = guid();
     const task: Task = { id, title, completed: false };
-    this.store.update(
-      stateProduce((state) => {
-        state.items.push(task);
-      }),
-    );
+
+    this.store.update((state) => {
+      const items = state.items.concat([task]);
+      return { ...state, items };
+    });
     return id;
   }
 
   removeTask(id: string) {
-    this.store.update(
-      stateProduce((state) => {
-        state.items = state.items.filter((item) => item.id !== id);
-      }),
-    );
+    this.store.update((state) => {
+      const items = state.items.filter((item) => item.id !== id);
+      return { ...state, items };
+    });
   }
 
   editTask(id: string, task: Partial<Omit<Task, 'id'>>) {
-    this.store.update(
-      stateProduce((state) => {
-        state.items = state.items.map((item) => {
-          return item.id === id ? { ...item, ...task } : item;
-        });
-      }),
-    );
+    this.store.update((state) => {
+      const items = state.items.map((item) => {
+        return item.id === id ? { ...item, ...task } : item;
+      });
+      return { ...state, items };
+    });
   }
 }
